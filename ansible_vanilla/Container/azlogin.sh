808083ddfe4204e745b58872dd4125aeeb83e580
John Hession <johhess@cdw.com>
2021-07-28 09:33:31 -0700

    Adding Ansible vanilla image based on Centos7

---
+++ ansible_vanilla/Container/azlogin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/azure-cli.repo
+
+yum install azure-cli -y
+
+az login -u <your username> -p <your password>
